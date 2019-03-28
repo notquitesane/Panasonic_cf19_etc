f64dc8c46313aad8fcfd69cf018fc819627a70f9
Joe Dufresne <notquitesane@gmail.com>
2019-03-28 03:10:03 -0700

    daily autocommit

--- fonts/infinality/conf.src/generate60/generate60.sh
+++ fonts/infinality/conf.src/generate60/generate60.sh
@@ -1,5 +1,1 @@
- #!/bin/sh
-
-awk -f 60-group-tt-fonts.conf.awk 60-group-tt-fonts.list > 60-group-tt-fonts.conf
-awk -f 60-group-non-tt-fonts.conf.awk 60-group-non-tt-fonts.list > 60-group-non-tt-fonts.conf
-
+# empty file because --include-config=n when `quickpkg` was used
